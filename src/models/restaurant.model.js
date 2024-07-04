9569da6a439265eb23da1cf4e2955df2f4958e74
ChisomSylvia <chisomsylvia95@gmail.com>
2024-07-04 16:18:35 +0100

    changed contact from number to string

--- src/models/restaurant.model.js
+++ src/models/restaurant.model.js
@@ -4,11 +4,13 @@ const RestaurantSchema = new Schema(
   {
     name: { type: String, required: true },
 
+    userId: { type: Schema.Types.ObjectId, required: true, ref: "user"},
+
     address: { type: String, required: true },
 
     description: { type: String, required: true },
 
-    contact: { type: Number, required: false },
+    contact: { type: String, required: false },
 
     image: { type: String, required: false },
   },
